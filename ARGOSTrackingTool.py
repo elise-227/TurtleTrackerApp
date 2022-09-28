1e08080b0692611cfda785ce9d0c303e8e5a5855
elise-227 <elise.boos@duke.edu>
2022-09-28 12:14:31 -0400

    add parsing code

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,3 +9,19 @@
 # Author: John Fay (john.fay@duke.edu)
 # Date:   Fall 2022
 #--------------------------------------------------------------
+
+# Copy and paste a line of data as the lineString variable value
+lineString = ""
+
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString
+
+# Assign variables to specfic items in the list
+record_id = lineData[]   # ARGOS tracking record ID
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[]       # Observation Location Class
+obs_lat = lineData[]     # Observation Latitude
+obs_lon = lineData[]     # Observation Longitude
+
+# Print information to the use
+print (f"Record {recordID} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
