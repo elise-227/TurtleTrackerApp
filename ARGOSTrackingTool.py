e54f781625f68e13138ea6a15937f7d934ddfd0b
elise-227 <elise.boos@duke.edu>
2022-10-04 21:04:27 -0400

    finish task 7

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -23,6 +23,8 @@ line_list = file_object.readlines()
 #close file
 file_object.close()
 
+user_date = input("Enter a date (M/D/YYYY):")
+
 #create empty dictionary 
 date_dict = {}
 location_dict = {}
@@ -47,6 +49,20 @@ for lineString in line_list:
     if obs_lc in ("1","2","3"):
         date_dict[record_id] = (obs_date)
         location_dict[record_id] = (obs_lat, obs_lon)
+     
+#Create an empty key list
+matching_keys = []
+        
+# Loop through all key, value pairs in the date_dictionary
+for the_key, the_value in date_dict.items():
+    #See if the date (the value) matches the user date
+    if the_value == user_date:
+        matching_keys.append(the_key)
+
+#Reveal locations for each key in matching_keys
+for matching_key in matching_keys:
+    obs_lat, obs_lon = location_dict[matching_key]
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}")
 
     #print the location of sara 
     #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
